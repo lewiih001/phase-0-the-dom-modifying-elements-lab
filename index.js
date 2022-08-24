c7c0a16c0390642300ce7c7bb620de55dec1a1e8
Lewis <lewismunyambu@gmail.com>
2022-08-24 14:00:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-newHeader.nodeName;{
-    
-}
+const newHeader=document.createElement('h1')
+newHeader.id='victory'
+newHeader.textContent='Lewis Ndila is the champion.'
+document.body.append(newHeader)
